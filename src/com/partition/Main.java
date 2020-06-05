a755280724adfb27cecda7ef4332c1b9297f8f3d
Anass LAKHLOUFI <anasslakhloufi@gmail.com>
2020-06-05 10:24:35 +0200

    Add custumizeTest function

--- src/com/partition/Main.java
+++ src/com/partition/Main.java
@@ -1,6 +1,7 @@
 package com.partition;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.partition.utils.Utils;
@@ -8,8 +9,13 @@ import com.partition.utils.Utils;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		int size;
+		try {
+			size = Integer.parseInt(args[args.length-1]);
+		}catch(Exception e) {
+			size = 2;
+		}
 		
-		int size = Integer.parseInt(args[args.length-1]);
 		List<String> list = new ArrayList<String>();
 		
 		for(int i=0;i<args.length-1;i++) {
@@ -20,6 +26,17 @@ public class Main {
 		for(List<String> l : result) {
 			System.out.println(l);
 		}
+		
+		// customizeTest();
 	}
 
+	
+	public static void customizeTest() throws Exception {
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7); //the initial list
+		List<List<Integer>> r = Utils.partition(list, 3); //partition function call with the initial list and the size of sublists
+		for(List<Integer> l : r) {
+			System.out.print(l);
+		}
+		System.out.println("Done");
+	}
 }
